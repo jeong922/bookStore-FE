64003842c9c79e2f653b0c76af63dd43608c165e
jeong922 <hwajeong922@naver.com>
2024-05-30 16:42:25 +0900

    Title 컴포넌트 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,11 @@
+import Title from '../components/common/Title';
+
 export default function Home() {
   return (
     <>
+      <Title size='large' color='background'>
+        제목 테스트
+      </Title>
       <div>home</div>
     </>
   );
