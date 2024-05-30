84791059ba0c6f1fa60032205a23da6e29036e8a
jeong922 <hwajeong922@naver.com>
2024-05-30 17:07:30 +0900

    Button 컴포넌트 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import Button from '../components/common/Button';
 import Title from '../components/common/Title';
 
 export default function Home() {
@@ -6,6 +7,9 @@ export default function Home() {
       <Title size='large' color='background'>
         제목 테스트
       </Title>
+      <Button size='large' scheme='primary'>
+        버튼 테스트
+      </Button>
       <div>home</div>
     </>
   );
