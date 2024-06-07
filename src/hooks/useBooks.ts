a5399d0ce182f3b80bed1dd12378b89598dbe8a2
jeong922 <hwajeong922@naver.com>
2024-06-07 21:37:51 +0900

    books useQuery 적용

--- src/hooks/useBooks.ts
+++ src/hooks/useBooks.ts
@@ -1,38 +1,32 @@
-import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
-import { Book } from '../models/book.model';
 import { fetchBooks } from '../api/books.api';
 import { QUERYSTRING } from '../constants/querystring';
 import { LIMIT } from '../constants/pagination';
-import { Pagination } from '../models/pagination.model';
+import { useQuery } from '@tanstack/react-query';
 
 export const useBooks = () => {
   const location = useLocation();
+  const params = new URLSearchParams(location.search);
 
-  const [books, setBooks] = useState<Book[]>([]);
-  const [pagination, setPagination] = useState<Pagination>({
-    totalCount: 0,
-    currentPage: 1,
+  const { data: booksData, isLoading: isBooksLoading } = useQuery({
+    queryKey: ['books', location.search],
+    queryFn: () =>
+      fetchBooks({
+        categoryId: params.get(QUERYSTRING.CATEGORY_ID)
+          ? Number(params.get(QUERYSTRING.CATEGORY_ID))
+          : undefined,
+        newBook: params.get(QUERYSTRING.NEWS) ? true : undefined,
+        page: params.get(QUERYSTRING.PAGE)
+          ? Number(params.get(QUERYSTRING.PAGE))
+          : 1,
+        maxResults: LIMIT,
+      }),
   });
-  const [isEmpty, setIsEmpty] = useState(true);
 
-  useEffect(() => {
-    const params = new URLSearchParams(location.search);
-    fetchBooks({
-      categoryId: params.get(QUERYSTRING.CATEGORY_ID)
-        ? Number(params.get(QUERYSTRING.CATEGORY_ID))
-        : undefined,
-      newBook: params.get(QUERYSTRING.NEWS) ? true : undefined,
-      page: params.get(QUERYSTRING.PAGE)
-        ? Number(params.get(QUERYSTRING.PAGE))
-        : 1,
-      maxResults: LIMIT,
-    }).then(({ books, pagination }) => {
-      setBooks(books);
-      setPagination(pagination);
-      setIsEmpty(books.length === 0);
-    });
-  }, [location.search]);
-
-  return { books, pagination, isEmpty };
+  return {
+    books: booksData?.books,
+    pagination: booksData?.pagination,
+    isEmpty: booksData?.books.length === 0,
+    isBooksLoading,
+  };
 };
