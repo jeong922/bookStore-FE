9ac51704ee041271630d25f88b2690150c819d4c
jeong922 <hwajeong922@naver.com>
2024-06-11 22:14:20 +0900

    상세페이지 이미지 모달창 구현

--- src/pages/BookDetail.tsx
+++ src/pages/BookDetail.tsx
@@ -10,6 +10,8 @@ import LikeButton from '../components/book/LikeButton';
 import AddToCart from '../components/book/AddToCart';
 import BookReview from '@/components/book/BookReview';
 import { Tab, Tabs } from '@/components/common/Tabs';
+import Modal from '@/components/common/Modal';
+import { useState } from 'react';
 
 const bookInfoList = [
   {
@@ -44,6 +46,8 @@ export default function BookDetail() {
   const { bookId } = useParams();
   const { book, likeToggle, reviews, addReview } = useBook(bookId);
 
+  const [isImgOpen, setIsImgOpen] = useState(false);
+
   if (!book) {
     return null;
   }
@@ -51,9 +55,12 @@ export default function BookDetail() {
   return (
     <BookDetailStyle>
       <header className='header'>
-        <div className='img'>
+        <div className='img' onClick={() => setIsImgOpen(true)}>
           <img src={getImgSrc(book.cover)} alt={book.title} className='img' />
         </div>
+        <Modal isOpen={isImgOpen} onClose={() => setIsImgOpen(false)}>
+          <img src={getImgSrc(book.cover)} alt={book.title} />
+        </Modal>
         <div className='info'>
           <Title size='large' color='text'>
             {book.title}
@@ -105,6 +112,10 @@ const BookDetailStyle = styled.div`
     align-items: start;
     gap: 24px;
     padding: 0 0 24px 0;
+
+    > div {
+      position: relative !important;
+    }
 
     .img {
       flex: 1;
