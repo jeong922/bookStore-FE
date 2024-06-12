619095819927bd3e93c6f88ecbe5b1c016aa5c5a
jeong922 <hwajeong922@naver.com>
2024-06-12 23:38:12 +0900

    메인 베너 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import Title from '@/components/common/Title';
+import Banner from '@/components/common/banner/Banner';
 import MainBest from '@/components/main/MainBest';
 import MainNewBooks from '@/components/main/MainNewBooks';
 import MainReview from '@/components/main/MainReview';
@@ -6,9 +7,11 @@ import { useMain } from '@/hooks/useMain';
 import styled from 'styled-components';
 
 export default function Home() {
-  const { reviews, newBooks, bestBooks } = useMain();
+  const { reviews, newBooks, bestBooks, banners } = useMain();
   return (
     <HomeStyle>
+      <Banner banners={banners} />
+
       <section className='section'>
         <Title size='large'>베스트 셀러</Title>
         <MainBest books={bestBooks} />
